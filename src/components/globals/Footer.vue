4a91a10c64a42d873f12b6dda428118f3d052687
Kanatani28 <5genbassnohito.maroro0208@gmail.com>
2020-02-22 21:07:31 +0900

    Add TopPage

---
+++ src/components/globals/Footer.vue
@@ -0,0 +1,13 @@
+<template>
+  <footer class="footer">
+    <div class="content has-text-centered">
+      <p class="has-text-black">
+        <a class="has-text-link" href="/"><strong>Advent Calendar Tsuku-ru</strong></a> by <a class="has-text-link" href="https://github.com/Kanatani28">maroKanatani</a>. 
+      </p>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {}
+</script>
--- src/components/pages/TopPage.vue
+++ src/components/pages/TopPage.vue
@@ -1,8 +1,39 @@
 <template>
-    <div>Hello TopPage</div>
+    <div class="has-text-white">
+        <section class="hero is-primary is-large">
+        <div class="hero-body">
+            <div class="container">
+                <h1 class="title is-size-1">
+                    アドベントカレンダーツクール
+                </h1>
+                <h2 class="subtitle">
+                    さあ、みんなでアウトプットをはじめよう
+                </h2>
+                <div class="content is-size-4">
+                    <p>クリスマスまでの期間に日数を数えるために使用されるアドベントカレンダー。<br>
+                    最近ではQiitaやブログなどの記事を投稿していくイベントを指す言葉としても有名ですよね。<br>
+                    そんなアドベントカレンダーをいつでも、気軽に作れるようにしてみました。
+                    </p>
+                </div>
+                
+                <router-link class="has-text-white" to="/new-calendar" exact>さっそくカレンダーを作ってみる</router-link>
+            </div>
+        </div>
+        </section>
+        <Footer />
+    </div>
 </template>
 
 <script>
+import Footer from '@/components/globals/Footer'
+
 export default {
+    components: {
+        Footer,
+    },
 }
 </script>
+
+<style scoped>
+
+</style>>
